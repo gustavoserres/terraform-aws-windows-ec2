f052c31d5ad8f888574946ce28c49a230aa2cb4a
gustavoserres <gusserres@gmail.com>
2022-08-29 17:56:40 +0100

    v2

--- network-variables.tf
+++ network-variables.tf
@@ -6,19 +6,19 @@
 variable "aws_az" {
   type        = string
   description = "AWS AZ"
-  default     = "eu-west-2c"
+  default     = "eu-west-2a"
 }
 
 # VPC Variables
 variable "vpc_cidr" {
   type        = string
   description = "CIDR for the VPC"
-  default     = "10.0.0.0/18"
+  default     = "10.99.0.0/16"
 }
 
 # Subnet Variables
 variable "public_subnet_cidr" {
   type        = string
   description = "CIDR for the public subnet"
-  default     = "10.1.0.0/24"
+  default     = "10.99.100.0/24"
 }
